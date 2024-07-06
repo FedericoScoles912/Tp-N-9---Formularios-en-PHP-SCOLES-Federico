783360d4dadd72b91e39c68939273b9117b32317
FedericoScoles912 <163576246+FedericoScoles912@users.noreply.github.com>
2024-07-06 17:40:11 -0300

    Update actividad2.php

---
+++ actividad2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Resultado</title>
+</head>
+<body>
+<?php
+$nombre = $_REQUEST['nombre'];
+$nivel_estudio = $_REQUEST['nivel_estudio'];
+
+echo "Nombre: " . $nombre . "<br>";
+echo "Nivel de estudios: " . $nivel_estudio;
+?>
+</body>
+</html>
